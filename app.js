53920ca75e962d5cc1e22f71f96c36960e626cd0
Aaron Niskin <amniskin@gmail.com>
2016-01-21 18:36:26 -0500

    fixed port issue

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+var port = 1234;
 var http = require('http');
 
 http.createServer(function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("Hello from the Node.js server!");
   response.end();
-}).listen(8080);
-console.log('Server is listening on port 8080');
+}).listen(port);
+console.log('Server is listening on port ' + port);
